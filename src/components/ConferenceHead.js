216badc285f1d37e15bb11a5ef4c451536592d10
Matt Kim <mattkim@matts-mbp.attlocal.net>
2018-03-19 00:33:11 -0700

    switch east and west colors

--- src/components/ConferenceHead.js
+++ src/components/ConferenceHead.js
@@ -9,7 +9,7 @@ export class ConferenceHead extends Component {
   render() {
 
     const { conf, toggleView, standingsView } = this.props;
-    const confBGColor = conf === "West" ? "#1D42BA" : "#C8102E" ;
+    const confBGColor = conf === "West" ? "#C8102E" : "#1D42BA" ;
     const cardinal = conf === "West" ? "Western" : "Eastern" ;
     const confButtonText = standingsView ? "tiles view" : "table view"
 
